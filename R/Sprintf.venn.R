f4d09518a581deeca8e375e59db070afa6ae8780
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-02 16:59:19 -0500

    test

--- R/Sprintf.venn.R
+++ R/Sprintf.venn.R
@@ -2,14 +2,12 @@
 
 #' @title Sprintf.venn
 #' 
-#' @param model a [venn] object
-#' 
-#' @param ... additional parameters, currently not in use
+#' @param x a [venn] object
 #' 
 #' @returns
 #' Function [Sprintf.venn] returns a \link[base]{character} scalar.
 #' 
 #' @export
-Sprintf.venn <- function(model, ...) {
-  sprintf(fmt = 'Venn diagram of %s are created using <u>**`R`**</u> package <u>**`VennDiagram`**</u>.', paste0('`', category_venn(model), '`', collapse = ', '))
+Sprintf.venn <- function(x) {
+  sprintf(fmt = 'Venn diagram of %s are created using <u>**`R`**</u> package <u>**`VennDiagram`**</u>.', paste0('`', category_venn(x), '`', collapse = ', '))
 }
